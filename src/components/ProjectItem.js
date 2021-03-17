f96b9b36ee36283db89ba42e394df61d2ff93243
“Aaron <github email address>
2021-03-17 18:17:44 -0400

    completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  // const technologyList = technologies.map((technology) => {
+  //   return <span key={project.id}>{technology.name}</span>
+  // });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {/* <p>{technologyList}</p> */}
       </div>
     </div>
   );
 }
 
-export default ProjectItem;
+export default ProjectItem; 
